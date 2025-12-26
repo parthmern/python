b788fd6eada35b4a309084d83b3d026b9b436f1f
parthmern <parthmern@gmail.com>
2025-12-25 23:20:30 -0500

    variable

--- 01_init.py
+++ 01_init.py
@@ -1,1 +1,13 @@
+import sys
 print("parth")
+print(sys.version)
+
+name="variable"
+age=20
+
+print(name)   # variabel
+name = "new name"   
+print(age)    # 20
+print(name)   # new name
+
+isAdult=True
