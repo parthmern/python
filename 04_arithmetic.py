85b713ecd0b770f4b70657ad6bc85a48d798e0f3
parthmern <parthmern@gmail.com>
2025-12-26 09:45:21 -0500

    arithmetic

---
+++ 04_arithmetic.py
@@ -0,0 +1,18 @@
+print(5/2)  # 2.5
+print(5**2) # power -> 25 
+
+i=2
+i+=2
+print(i) # 4
+
+# operator precedance 
+# BODMAS rule
+print( 2 + 3 * 9 )  #29
+
+print( 3 > 2 )  # True
+print( 3 == 3 ) # True
+print( 3 != 3 ) # False
+
+print( 2>3 or 2>1 ) #True
+print( 2>3 and 2>1 ) #False
+print( not 2>3 ) #True
