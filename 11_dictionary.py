b42741c98004bcda79d0cafdb5cf42245b12b495
parthmern <parthmern@gmail.com>
2025-12-26 10:15:19 -0500

    dictionary

---
+++ 11_dictionary.py
@@ -0,0 +1,8 @@
+marks={"parth": 31, "ram": 62, "hero": 33}
+
+print(marks["parth"])   # 31
+
+marks["newadded"] = 23
+marks["parth"] = 99
+
+print(marks)    # {'parth': 99, 'ram': 62, 'hero': 33, 'newadded': 23}
