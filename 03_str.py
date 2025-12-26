7f9f3ca68bf39175b896dd94e1201adaca199212
parthmern <parthmern@gmail.com>
2025-12-26 09:38:17 -0500

    String

---
+++ 03_str.py
@@ -0,0 +1,13 @@
+name="parth"
+capital = name.upper()
+print(name) # part
+print(capital)  # PART
+
+print( name.find('s') ) # -1        # in python nothing like char so you can use "" double quote as well
+print( name.find('rt') ) # 2 index
+print( name.find("r") ) # 2 index
+
+print( name.replace("rt", "replaced") ) # pareplacedh
+
+print( "par" in name )  # True
+print( "xx" in name )   # False
