e52880475f5db24224b9e20fb5c89632d1b487d3
parthmern <parthmern@gmail.com>
2025-12-25 23:27:30 -0500

    input

---
+++ 02_input.py
@@ -0,0 +1,13 @@
+name = input("what is name")
+print("entered name is ", name)
+
+oldAge = input("old age?")
+oldAge = int(oldAge)
+newAge = oldAge + 2
+print(f"New age is {newAge}")
+
+# type conversions
+# int()
+# float()
+# str()
+# bool()
