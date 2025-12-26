64b07344bbfeeeb589beb3e4840f7c65ce7016e6
parthmern <parthmern@gmail.com>
2025-12-26 09:49:15 -0500

    ifelse

---
+++ 05_ifElse.py
@@ -0,0 +1,11 @@
+age = 1
+
+if age>=18:
+    print("you are adult")
+    print("you can vote")
+elif age<18 and age>3:
+    print("you are in school")
+else:
+    print("you are child")
+
+print("end of code")
