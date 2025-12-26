c4699ee4134b10079e1b332f8719a95181032cea
parthmern <parthmern@gmail.com>
2025-12-26 10:21:24 -0500

    functions

---
+++ 12_functions.py
@@ -0,0 +1,25 @@
+# inbuilt
+# int() , str() , bool()
+
+# ------------------------
+# module functions
+
+import math
+print(dir(math)) # print all funcs  in this module
+
+# ['__doc__', '__loader__', '__name__', '__package__', '__spec__', 'acos', 'acosh', 'asin', 'asinh', 'atan', 'atan2', 'atanh', 'cbrt', 'ceil', 'comb', 'copysign', 'cos', 'cosh', 'degrees', 'dist', 'e', 'erf', 'erfc', 'exp', 'exp2', 'expm1', 'fabs', 'factorial', 'floor', 'fma', 'fmod', 'frexp', 'fsum', 'gamma', 'gcd', 'hypot', 'inf', 'isclose', 'isfinite', 'isinf', 'isnan', 'isqrt', 'lcm', 'ldexp', 'lgamma', 'log', 'log10', 'log1p', 'log2', 'modf', 'nan', 'nextafter', 'perm', 'pi', 'pow', 'prod', 'radians', 'remainder', 'sin', 'sinh', 'sqrt', 'sumprod', 'tan', 'tanh', 'tau', 'trunc', 'ulp']
+
+from math import sqrt
+print(sqrt(25)) # 5.0
+
+# from math import *  
+# * to import everything
+
+# ------------------------
+# user defined func
+
+def func_name(param1, param2 = 99):
+    print(param1+param2)
+
+func_name(1,2)  # 3
+func_name(1)    # 100
