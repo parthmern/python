38479ea46bb890df32966888f6f629ab7fc3606b
parthmern <parthmern@gmail.com>
2025-12-26 09:53:43 -0500

    loop

---
+++ 06_loop.py
@@ -0,0 +1,21 @@
+i=1
+while i<=5:
+    print(i * "*")
+    i+=1
+
+
+# *
+# **
+# ***
+# ****
+# *****
+
+# i + "*" throw error because you are trying to concat string here
+
+
+# 0 ->  <2
+for i in range(2):
+    print(f"Iteration number: {i}")
+
+# Iteration number: 0
+# Iteration number: 1
